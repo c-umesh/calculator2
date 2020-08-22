ff8d68124f984797d07b66012129e3ca78edc9b5
c-umesh <umesh.c@gmail.com>
2020-08-22 11:48:08 +0530

    docstring fixed

--- calculator.py
+++ calculator.py
@@ -1,9 +1,12 @@
 """
 Calculator library containing basic math operations.
 """
+
+
 def add(first_term, second_term):
     """ add two numbers"""
     return first_term + second_term
+
 
 def subtract(first_term, second_term):
     """ substracts two numbers"""
