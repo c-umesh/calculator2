8bbe287af271dfa32a10bc3325c225ee7d38608a
c-umesh <umesh.c@gmail.com>
2020-08-22 11:37:50 +0530

    test for linting

--- calculator.py
+++ calculator.py
@@ -1,14 +1,10 @@
 """
 Calculator library containing basic math operations.
 """
-
-
 def add(first_term, second_term):
     """ add two numbers"""
     return first_term + second_term
 
-
 def subtract(first_term, second_term):
     """ substracts two numbers"""
     return first_term - second_term
-    
